2893732cf55b5fab8123bdafcd26ccbe530c61f6
Hilla Barkal <hilla.guz@gmail.com>
2017-07-07 11:00:56 +0300

    adding column to everything

--- B17 Ex05/Ex05.GUI/BoardForm.cs
+++ B17 Ex05/Ex05.GUI/BoardForm.cs
@@ -13,6 +13,7 @@ namespace Ex05.GUI
         private List<List<GuessButton>> m_GuessRows = new List<List<GuessButton>>();
         private List<GuessButton> m_ArrowButtons = new List<GuessButton>();
         private List<List<Button>> m_ScoreButtons = new List<List<Button>>();
+        private List<List<char>> m_PlayersGuesses = new List<List<char>>();
         private int m_CurrentRound = 0;
         private Game m_CurrentGame;
 
@@ -61,7 +62,7 @@ namespace Ex05.GUI
                 List<GuessButton> buttonsInRow = new List<GuessButton>();
                 for (int colum = 0; colum < k_NumberOfButtonsInGuess; colum++)
                 {
-                    GuessButton grayButton = new GuessButton(row);
+                    GuessButton grayButton = new GuessButton(row, colum);
                     grayButton.Location = new Point(15 + (45 * colum), 80 + (45 * row));
                     buttonsInRow.Add(grayButton);
                     controls.Add(grayButton);
@@ -93,7 +94,7 @@ namespace Ex05.GUI
             List<Button> scoreButtons = new List<Button>();
             for (int i = 0; i < k_NumberOfButtonsInGuess; i++)
             {
-                GuessButton scoreButton = new GuessButton(i_RowNumber);
+                GuessButton scoreButton = new GuessButton(i_RowNumber, i);
                 scoreButton.Size = new Size(15, 15);
                 scoreButton.Location = new Point(this.ClientSize.Width - scoreButton.Width - 10 - (20 * (i % 2)),
                                                  80 + (20 * (i / 2) + (45 * i_RowNumber)));
@@ -135,8 +136,11 @@ namespace Ex05.GUI
             GuessButton guessButton = sender as GuessButton;
             if (guessButton != null)
             {
-                PickAColorForm colorsForm = new PickAColorForm(guessButton.Row);
-
+                PickAColorForm colorsForm = new PickAColorForm(guessButton.Row, guessButton.Colum,  m_PlayersGuesses[guessButton.Row]);
+                colorsForm.ShowDialog();
+                m_PlayersGuesses[guessButton.Row] = colorsForm.Guess;
+                //guessButton.Guess = 
+                //colorsForm.Close();
             }
         }
 
@@ -153,15 +157,15 @@ namespace Ex05.GUI
             }
         }
 
-        private List<char> getCurrentGuess()
-        {
-            List<char> guess = new List<char>();
-            for (int i = 0; i < k_NumberOfButtonsInGuess; i++)
-            {
-                guess.Add(m_GuessRows[m_CurrentRound][i].Guess);
-            }
-            return guess;
-        }
+        //private List<char> getCurrentGuess()
+        //{
+        //    List<char> guess = new List<char>();
+        //    for (int i = 0; i < k_NumberOfButtonsInGuess; i++)
+        //    {
+        //        guess.Add(m_GuessRows[m_CurrentRound][i].Guess);
+        //    }
+        //    return guess;
+        //}
 
         public void ActivateRow(int i_RowNumber)
         {
@@ -172,6 +176,7 @@ namespace Ex05.GUI
             }
 
             m_ArrowButtons[i_RowNumber].Enabled = true;
+            m_PlayersGuesses[i_RowNumber] = new List<char>();
         }
 
         private void showScore(int i_Row, int i_CorrectInPlace, int i_CorrectMissPlace)
@@ -193,11 +198,22 @@ namespace Ex05.GUI
     {
         private List<GuessButton> m_ColoredButtons = new List<GuessButton>();
         private List<char> m_Guess = new List<char>();
-        private int m_Round;
-
-        public PickAColorForm(int i_Round) : base()
+        private int m_Row;
+        private int m_Colum;
+		
+		public List<char> Guess
+		{
+			get
+			{
+				return m_Guess;
+			}
+		}
+		
+        public PickAColorForm(int i_Row, int i_Colum, List<char> i_Guess) : base()
         {
-            this.m_Round = i_Round;
+            this.m_Guess = i_Guess;
+            this.m_Row = i_Row;
+            this.m_Colum = i_Colum;
             this.Size = new Size(190, 100);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -207,11 +223,11 @@ namespace Ex05.GUI
         protected override void OnShown(EventArgs e)
         {
             base.OnShown(e);
-            initControls(m_Round);
+            initControls(m_Row, m_Colum);
             setControlsEventHandlers();
         }
 
-        protected void initControls(int i_Round)
+        protected void initControls(int i_Row, int i_Colum)
         {
             Color[] buttonsColors = { Color.Pink, Color.Red, Color.LightGreen,
                                       Color.LightBlue, Color.Blue, Color.Yellow,
@@ -219,10 +235,14 @@ namespace Ex05.GUI
             for (int i = 0; i < 8; i++)
             {
                 char currentGuess = (char)('A' + i);
-                GuessButton currentButton = new GuessButton(i_Round, currentGuess);
+                GuessButton currentButton = new GuessButton(i_Row, i_Colum, currentGuess);
                 currentButton.BackColor = buttonsColors[i];
-                currentButton.Location = new Point(5 + ((i % 2) * 45),
-                                                   5 + ((i / 2) * 45));
+                currentButton.Location = new Point(5 + ((i / 2) * 45),
+                                                   5 + ((i % 2) * 45));
+                if (m_Guess.Contains(currentGuess))
+                {
+                    currentButton.Enabled = false;
+                }
 
                 m_ColoredButtons.Add(currentButton);
             }
@@ -244,8 +264,8 @@ namespace Ex05.GUI
             if (colorButton != null)
             {
                 m_Guess.Add(colorButton.Guess);
-                colorButton.Enabled = false;
             }
+            //this.Close();
         }
 
 
@@ -263,6 +283,7 @@ namespace Ex05.GUI
 
     public class GuessButton : SequenceButton
     {
+        private int m_Colum;
         private int m_Row;
         private char m_Guess;
 
@@ -272,9 +293,20 @@ namespace Ex05.GUI
             {
                 return m_Guess;
             }
-
+            internal set
+            {
+                m_Guess = value;
+            }
         }
-
+		
+		public int Colum
+		{
+			get
+			{
+				return m_Colum;
+			}
+		}
+		
         public int Row
         {
             get
@@ -283,66 +315,69 @@ namespace Ex05.GUI
             }
         }
 
-        //     private List<char> m_Guess;
+		//     private List<char> m_Guess;
 
-        //     public List<char> Guess
-        //     {
-        //         get
-        //         {
-        //             return m_Guess;
-        //         }
-        //         set
-        //         {
-        //	List<char> userGuess = new List<char>();
-        //	bool validGuess = false;
+		//     public List<char> Guess
+		//     {
+		//         get
+		//         {
+		//             return m_Guess;
+		//         }
+		//         set
+		//         {
+		//	List<char> userGuess = new List<char>();
+		//	bool validGuess = false;
 
-        //	while (!validGuess)
-        //	{
-        //		char currentInputLetter;
-        //		int letterCounter = 0;
-        //		bool validLetter = true;
+		//	while (!validGuess)
+		//	{
+		//		char currentInputLetter;
+		//		int letterCounter = 0;
+		//		bool validLetter = true;
 
-        //                 for (int i = 0; i < value.Count && validLetter; i += 2)
-        //		{
-        //                     currentInputLetter = value[i];
-        //			letterCounter++;
+		//                 for (int i = 0; i < value.Count && validLetter; i += 2)
+		//		{
+		//                     currentInputLetter = value[i];
+		//			letterCounter++;
 
-        //			if (userGuess.Contains(currentInputLetter))
-        //			{
-        //				throw new ArgumentException("Please use each letter only once");
-        //			}
-        //			else
-        //			{
-        //				userGuess.Add(currentInputLetter);
-        //			}
-        //		}
+		//			if (userGuess.Contains(currentInputLetter))
+		//			{
+		//				throw new ArgumentException("Please use each letter only once");
+		//			}
+		//			else
+		//			{
+		//				userGuess.Add(currentInputLetter);
+		//			}
+		//		}
 
-        //                 if (letterCounter != 4 || !validLetter)
-        //		{
-        //			validGuess = false;
-        //			userGuess.Clear();
-        //		}
-        //		else
-        //		{
-        //			validGuess = true;
-        //		}
-        //	}
+		//                 if (letterCounter != 4 || !validLetter)
+		//		{
+		//			validGuess = false;
+		//			userGuess.Clear();
+		//		}
+		//		else
+		//		{
+		//			validGuess = true;
+		//		}
+		//	}
 
-        //             m_Guess = userGuess;
-        //}
-        //}
+		//             m_Guess = userGuess;
+		//}
+		//}
 
-        public GuessButton(int i_Row) : base()
+		public GuessButton(int i_Row) : base()
+		{
+			m_Row = i_Row;
+			this.BackColor = Color.Gray;
+		}
+
+        public GuessButton(int i_Row, int i_Colum) : this(i_Row)
         {
-            m_Row = i_Row;
-            this.BackColor = Color.Gray;
+            m_Colum = i_Colum;   
         }
 
-        public GuessButton(int i_Row, char i_Guess) : base()
+        public GuessButton(int i_Row, int i_Colum, char i_Guess) : this(i_Row, i_Colum)
         {
-            m_Row = i_Row;
             m_Guess = i_Guess;
-            this.BackColor = Color.Gray;
         }
     }
 }
