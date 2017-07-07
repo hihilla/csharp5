e0d856fa96265575d6b9b535841563244ea9dc12
Hilla Barkal <hilla.guz@gmail.com>
2017-07-07 15:20:19 +0300

    i dont know

--- B17 Ex05/Ex05.GUI/Manager.cs
+++ B17 Ex05/Ex05.GUI/Manager.cs
@@ -5,13 +5,13 @@ namespace Ex05.GUI
 {
     public class Manager
     {
-        private int m_PlayersNumberOfRounds;
-        private int m_CurrentRound;
+        //private int m_PlayersNumberOfRounds;
+        //private int m_CurrentRound;
         private string m_GamesWord;
         private List<List<char>> m_ListOfPlayerGuesses = new List<List<char>>();
         private List<List<char>> m_ListOfGuessesFeedback = new List<List<char>>();
-        private bool m_PlayerWins = false;
-        private bool m_KeepPlaying = true;
+        //private bool m_PlayerWins = false;
+        //private bool m_KeepPlaying = true;
  
         public Manager()
         {
@@ -19,7 +19,7 @@ namespace Ex05.GUI
 
         public void Run()
         {
-            m_KeepPlaying = true;
+            //m_KeepPlaying = true;
 
         }
 
@@ -34,11 +34,6 @@ namespace Ex05.GUI
             int numberOfGuesses = guessForm.CounterOfGuessesClicks;
             BoardForm board = new BoardForm(numberOfGuesses, game);
             board.ShowDialog();
-
-            for (m_CurrentRound = 1; m_CurrentRound < m_PlayersNumberOfRounds + 1 && !player.QuiteGame && !m_PlayerWins; m_CurrentRound++)
-            {
-                board.ActivateRow(m_CurrentRound);
-            } 
         }
     }
 }
